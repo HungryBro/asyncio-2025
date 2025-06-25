c6820cc7aa13fce7184415bd759c9637138670e5
Kritchai <loma.kcps@gmail.com>
2025-06-25 11:11:45 +0700

    New ass4 asyncio04 reversed 2 1 0

--- assignment04/asyncio04.py
+++ assignment04/asyncio04.py
@@ -13,12 +13,16 @@ async def main():
     # report a message
     print(f"{time.ctime()} main coroutine started")
     # start mant tasks
-    started_tasks = [asyncio.create_task(dowload_image(i, i)) for i in range(3)]
+    # started_tasks = [asyncio.create_task(dowload_image(i, i)) for i in range(3)]
+
+    # โหลดแบบย้อนกลับโดยใช้ reversed
+    started_tasks = [asyncio.create_task(dowload_image(i, delay)) for i, delay in zip(reversed(range(3)), range(3))]
 
     # allow some of the tasks time to start
     await asyncio.sleep(0.1)
     for task in started_tasks:
         await task
 
+        
 # start the asyncio program
 asyncio.run(main())
